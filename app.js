bb8a152b83a1f68f2bc251f2381c3ec9cf96c12e
preethi-aleti <Aleti.Preethi@cognizant.com>
2025-10-16 14:53:36 +0530

    changes in reservation

--- app.js
+++ app.js
@@ -1,36 +1,61 @@
+// const express = require('express');
+// const cors = require('cors');
+// const morgan = require('morgan');
+// const helmet = require('helmet');
+// const { errorHandler } = require('./src/middleware/errorHandler');
+
+// const app = express();
+
+// // Middleware
+// app.use(helmet()); // Security headers
+// app.use(morgan('dev')); // Logging
+// app.use(cors()); // Enable CORS
+// app.use(express.json()); // Body parser
+// app.use(express.urlencoded({ extended: true }));
+
+// // Health check route
+// app.get('/health', (req, res) => {
+//   res.status(200).json({ status: 'OK', message: 'Server is running' });
+// });
+
+// // Import routes
+// const parkingRoutes = require('./src/routes/parkingRoutes');
+
+// // Routes
+// app.use('/api/v1/parking-spots', parkingRoutes);
+
+// // Error handling
+// app.use((req, res, next) => {
+//   const error = new Error('Not Found');
+//   error.status = 404;
+//   next(error);
+// });
+
+// app.use(errorHandler);
+
+// module.exports = app;
+require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const { errorHandler } = require('./src/middleware/errorHandler');
+
+
+const userRoutes = require('./src/routes/userRoutes');
+
+const errorHandler = require('./src/middleware/errorHandler');
+
+
+const { Security } = require('./src/middleware/headers');
+const logger = require('./src/middleware/logger');
+const cspHeader = require('./src/middleware/cspheader');
 
 const app = express();
 
-// Middleware
-app.use(helmet()); // Security headers
-app.use(morgan('dev')); // Logging
-app.use(cors()); // Enable CORS
-app.use(express.json()); // Body parser
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Health check route
-app.get('/health', (req, res) => {
-  res.status(200).json({ status: 'OK', message: 'Server is running' });
-});
+app.use(logger)
+app.use(cspHeader)
+app.use('/api', userRoutes);
 
-// Import routes
-const parkingRoutes = require('./src/routes/parkingRoutes');
 
-// Routes
-app.use('/api/v1/parking-spots', parkingRoutes);
-
-// Error handling
-app.use((req, res, next) => {
-  const error = new Error('Not Found');
-  error.status = 404;
-  next(error);
-});
-
-app.use(errorHandler);
-
+app.use(errorHandler) // Error Handler
+app.use(Security)
 module.exports = app;
---
+++ src/middleware/cspheader.js
@@ -0,0 +1,7 @@
+const cspHeader = (req, res, next) => {
+  res.setHeader("Content-Security-Policy",
+    "default-src 'self'; script-src 'self' https://trusted.cdn.com; object-src 'none'; base-uri 'self'; frame-ancestors 'none';");
+  next();
+};
+
+module.exports = cspHeader;
