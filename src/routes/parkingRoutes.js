99741c45a095b3691540359d222eb4edef87f8e8
vathsav-cogni <Bukka.SaiSriVathsav@cognizant.com>
2025-11-06 08:59:38 +0530

    small changes

--- src/routes/parkingRoutes.js
+++ src/routes/parkingRoutes.js
@@ -62,4 +62,40 @@ router.delete('/:id',
 );
 
 
+// router.get('/',
+//   getAllParkingSpots
+// );
+
+// // Get available parking spots
+// router.get('/available-slots',
+//   getAvailableSlots
+// );
+
+// // Add new parking spot
+// router.post('/', 
+//   validateAddParkingSpot,
+//   addParkingSpot
+// );
+
+// // Get specific parking spot
+// router.get('/:id',
+//   validateParkingSpotId, 
+//   getParkingSpotById
+// );
+
+// // Update parking spot status
+// router.put('/:id/status',
+//   validateParkingSpotId, 
+//   updateParkingSpotStatus
+// );
+
+// // remove parking spot
+// router.delete('/:id', 
+//   validateParkingSpotId, 
+//   deleteParkingSpot
+// );
+
+
+
+
 module.exports = router;
