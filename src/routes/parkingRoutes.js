2d33d85a53cc3ce7150c555d225482e2ed83b865
vathsav-cogni <Bukka.SaiSriVathsav@cognizant.com>
2025-11-07 13:58:29 +0530

    Router update and test case

--- src/routes/parkingRoutes.js
+++ src/routes/parkingRoutes.js
@@ -19,80 +19,44 @@ const {
 } =  require('../middleware/parkingValidationMiddleware');
 
 // Get all parking spots
-// router.get('/',
-//   passport.authenticate('jwt', { session: false }),
-//   getAllParkingSpots
-// );
-
-// // Get available parking spots
-
-
-// // Add new parking spot
-// router.post('/', 
-//   passport.authenticate('jwt', { session: false }),
-//   requireRole('admin'),
-//   validateAddParkingSpot,
-//   addParkingSpot
-// );
-
-// // Get specific parking spot
-// router.get('/:id',
-//   passport.authenticate('jwt', { session: false }),
-//   validateParkingSpotId, 
-//   getParkingSpotById
-// );
-
-// // Update parking spot status
-// router.put('/:id/status', 
-//   passport.authenticate('jwt', { session: false }),
-//   requireRole('admin'),
-//   validateParkingSpotId, 
-//   updateParkingSpotStatus
-// );
-
-// // remove parking spot
-// router.delete('/:id', 
-//   passport.authenticate('jwt', { session: false }),
-//   requireRole('admin'),
-//   validateParkingSpotId, 
-//   deleteParkingSpot
-// );
-
-
 router.get('/',
+  passport.authenticate('jwt', { session: false }),
   getAllParkingSpots
 );
 
 // Get available parking spots
-router.get('/available-slots',
-  getAvailableSlots
-);
+
 
 // Add new parking spot
 router.post('/', 
+  passport.authenticate('jwt', { session: false }),
+  requireRole('admin'),
   validateAddParkingSpot,
   addParkingSpot
 );
 
 // Get specific parking spot
 router.get('/:id',
+  passport.authenticate('jwt', { session: false }),
   validateParkingSpotId, 
   getParkingSpotById
 );
 
 // Update parking spot status
-router.put('/:id/status',
+router.put('/:id', 
+  passport.authenticate('jwt', { session: false }),
+  requireRole('admin'),
   validateParkingSpotId, 
   updateParkingSpotStatus
 );
 
 // remove parking spot
 router.delete('/:id', 
+  passport.authenticate('jwt', { session: false }),
+  requireRole('admin'),
   validateParkingSpotId, 
   deleteParkingSpot
 );
-
-
 
 
 module.exports = router;
