f1ef1bd116b8148f9d326d518d944fcb12f6fd77
rohi-ps <rohit.sudarsanan@cognizant.com>
2025-11-13 10:24:19 +0530

    passport

--- src/routes/reservationRoutes.js
+++ src/routes/reservationRoutes.js
@@ -9,26 +9,26 @@ const { reservationValidators, updateReservationValidators } = require('../valid
  
 const { requireRole } = require("../middleware/jwt.js");
 
-router.post('/create', 
+router.post('/create',  passport.authenticate('jwt', { session: false }),
   reservationValidators, async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
     await createReservation(req, res);
   });
 
-router.put('/update/:id', 
+router.put('/update/:id',  passport.authenticate('jwt', { session: false }),
   updateReservationValidators, async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
     await updateReservation(req, res);
   });
 
-router.delete('/delete/:id', 
+router.delete('/delete/:id', passport.authenticate('jwt', { session: false }),
   deleteReservation);
 
-router.get('/user/:userId',
+router.get('/user/:userId', passport.authenticate('jwt', { session: false }),
   getReservationByUser);
-router.get('/getall', 
+router.get('/getall', passport.authenticate('jwt', { session: false }),
   allusers);
 
 module.exports = router;
